63cf2138ce9f63cb8ad83819ba4e2dedf4c33fbb
Lrop <Lagropner04@gmail.com>
2019-09-15 15:51:29 -0600

    save npm packages/sever running

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
-const express = require ("express");
-      mongoose = require("mongoose");
-      exphbs = require ("express-handlebars");
-      bodyParser = require ("body-parser");
-      cheerio = require("cheerio");
-      axios = require("axios");
+// Requiring our dependencies 
 
 
-var db = require('./Scrapin-App/models')
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var axios = require("axios");
+var morgan = require('morgan')
+
+
+// app.use (logger("dev"));
+// app.use (
+//     bodyParser.urlencoded({
+//         extended : false
+//     })
+
+// )
+
+var app = express ();
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    // console.log("Listening on Port" + port);
+    console.log("Listening on " + " http://localhost:3000");
+
+
+});
+
+
+
