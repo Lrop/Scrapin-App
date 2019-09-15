8b868f98ecb7751c02771d2e7ac117f24a3c90a4
Lrop <Lagropner04@gmail.com>
2019-09-15 15:26:46 -0600

    first

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+      mongoose = require("mongoose");
+      exphbs = require ("express-handlebars");
+      bodyParser = require ("body-parser");
+      cheerio = require("cheerio");
+      axios = require("axios");
+
+
+var db = require('./Scrapin-App/models')
